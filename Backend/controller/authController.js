ff874286e98d1e5f2aeb275d69736a558bff1835
Abhishek Dobhal <abhishekdobhal66@gmail.com>
2025-09-01 13:40:30 +0530

    Created signup, login and logout controllers

---
+++ Backend/controller/authController.js
@@ -0,0 +1,88 @@
+import User from "../model/userModel.js"
+import validator from 'validator'
+import bcrypt from "bcryptjs"
+import genToken from "../config/token.js"
+
+export const signup = async (req, res) => {
+    try {
+        const { name, email, password, role} = req.body
+        let existUser = await User.findOne({email})
+        if(existUser) {
+            return res.status(400).json({
+                message : "User already exists"
+            })
+        }
+        if(!validator.isEmail(email)){
+            return res.status(400).json({
+                message : "Enter valid email"
+            })
+        }
+        if(password.length < 8) {
+            return res.status(400).json({
+                message : "Enter strong password"
+            })
+        }
+        let hashPassword = await bcrypt.hash(password, 10)
+        const user = await User.create({
+            name,
+            email,
+            password : hashPassword,
+            role
+        })
+        const token = genToken(user._id)
+        res.cookie("token", token, {
+            httpOnly : true,
+            secure : false,
+            sameSite : "strict",
+            maxAge : 7 * 24 * 60 * 60 * 1000
+        })
+        return res.status(201).json(user)
+    } catch (error) {
+        return res.status(500).json({
+            message : `Signup error : ${error}`
+        })
+    }
+}
+
+export const login = async (req,res) => {
+    try {
+        const {email , password} = req.body
+        let user = await User.findOne({email})
+        if(!user) {
+            return res.status(404).json({
+                message : "User not found"
+            })
+        }
+        let isMatch = await bcrypt.compare(password, user.password)
+        if(!isMatch){
+            return res.status(400).json({
+                message : "Incorrect password"
+            })
+        }
+        const token = genToken(user._id)
+        res.cookie("token", token, {
+            httpOnly : true,
+            secure : false,
+            sameSite : "strict",
+            maxAge : 7 * 24 * 60 * 60 * 1000
+        })
+        return res.status(200).json(user)
+    } catch (error) {
+        return res.status(500).json({
+            message : `Login error : ${error}`
+        })
+    }
+}
+
+export const logOut = async (req,res) => {
+    try {
+        await res.clearCookie("token")
+        return res.status(200).json({
+            message : "Logout successful"
+        })
+    } catch (error) {
+        return res.status(500).json({
+            message : `Logout error : ${error}`
+        })
+    }
+}
--- Backend/index.js
+++ Backend/index.js
@@ -1,10 +1,14 @@
 import express from "express";
 import dotenv from "dotenv";
 import connectDb from "./config/connectDB.js";
+import cookieParser from "cookie-parser";
 dotenv.config();
 
 const app = express();
 const port = process.env.PORT;
+
+app.use(express.json())
+express.use(cookieParser())
 
 app.get("/", (req, res) => {
   res.send("hello");
